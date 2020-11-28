88e96650b4e4fe41056a27122f1c36fb82d6d8ad
Ximena Castañeda <xcastaneda@unal.edu>
2020-11-28 12:15:37 -0500

    Búsquedas.

--- src/main/java/org/example/Controladores/MenuBusqueda.java
+++ src/main/java/org/example/Controladores/MenuBusqueda.java
@@ -19,33 +19,37 @@ public class MenuBusqueda implements Initializable {
     //Se debe especificar el tipo de dato que serán las opciones para que no se pueda agregar cualquier cosa.
     public ComboBox<String> entidad;
     public ComboBox<String> atributo;
+  //public ComboBox<String> relaciones;
     public TextField valor;
     public TextArea resultados;
     public Label novedad;
     public Label EtiquetaValor;
+    //public Label EtiqRelaciones;
     String selecEntidad;
     String valorBusq;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         entidad.setItems(FXCollections.observableArrayList ("Clinica","Paciente","Psiquiatra"));
+        novedad.setText("Por favor tenga en cuenta las tildes y espacios para ingresar el valor de búsqueda.");
     }
 
-    public void EscogerEntidad(ActionEvent event){
+    public void itemsAtributo(ActionEvent event){
         selecEntidad = String.valueOf(entidad.getValue());
         switch (selecEntidad) {
             case "Clinica":
                 atributo.setItems(FXCollections.observableArrayList("Nit", "Nombre", "Teléfono","Psiquiatra","Paciente"));
                 break;
             case "Paciente":
-                atributo.setItems(FXCollections.observableArrayList("Cédula", "Apellido", "Edad","Clinica","Psquiatra"));
+                atributo.setItems(FXCollections.observableArrayList("Cédula", "Apellido", "Edad","Clinica","Psiquiatra"));
+                break;
             case "Psiquiatra":
                 atributo.setItems(FXCollections.observableArrayList("Cédula", "Apellido", "Edad","Clinica","Paciente"));
                 break;
         }
     }
 
-    public void EscogerCriterio(ActionEvent event){
+    public void mensajeValor(ActionEvent event){
         switch (String.valueOf(atributo.getValue())) {
             case "Psiquiatra":
                 EtiquetaValor.setText("Cédula psiquiatra: ");
@@ -65,7 +69,8 @@ public class MenuBusqueda implements Initializable {
     public void buscar(ActionEvent event) {
         resultados.setEditable(false);
         valorBusq = "Dato no encontrado";
-        // resultados.clear();
+        novedad.setText("");
+        resultados.clear();
 
         if (entidad.getValue() == null || atributo.getValue() == null || valor.getText().equals("")){
             novedad.setText("Campos vacíos, por favor diligencie la información.");
@@ -100,7 +105,7 @@ public class MenuBusqueda implements Initializable {
                         return;
                     }
                 } catch (Exception excep) {
-                    novedad.setText("El NIT es un número, por favor verifique el valor ingresado.");
+                    novedad.setText("Dato inválido,por favor verifique el valor ingresado.");
                     valorBusq = "Dato inválido.";
                     return;
                 }
@@ -113,7 +118,7 @@ public class MenuBusqueda implements Initializable {
                 for (String clave : Clinica.clinicaNom.keySet()) {
                     if (clave.contains(valor.getText().trim().toLowerCase())) {
                         valorBusq = clave;
-                        resultados.appendText(imprimirList(Clinica.clinicaNom.get(valorBusq).toString()));
+                        resultados.appendText(imprimirLista(Clinica.clinicaNom.get(valorBusq).toString()));
                     }
                 }
                 break;
@@ -132,7 +137,7 @@ public class MenuBusqueda implements Initializable {
                 for (Integer clave : Clinica.clinicaTel.keySet()) {
                     if (String.valueOf(clave).contains(valor.getText().trim())) {
                         valorBusq = String.valueOf(clave);
-                        resultados.appendText(imprimirList(Clinica.clinicaTel.get(Integer.parseInt(valorBusq)).toString()));
+                        resultados.appendText(imprimirLista(Clinica.clinicaTel.get(Integer.parseInt(valorBusq)).toString()));
                     }
                 }
                 break;
@@ -145,16 +150,16 @@ public class MenuBusqueda implements Initializable {
                         return;
                     }
                 } catch (Exception excep) {
-                    novedad.setText("La cédula es un número, por favor verifique el valor ingresado.");
+                    novedad.setText("Dato inválido, por favor verifique el valor ingresado.");
                     valorBusq = "Dato inválido";
                     return;
                 }
                 Psiquiatra buscado = null;
-                if (Psiquiatra.psiquiatraHash.containsKey(valor.getText()) && atributo.getValue().equals("Psiquiatra")){
-                    valorBusq = valor.getText();
+                if (Psiquiatra.psiquiatraHash.containsKey(valor.getText().trim()) && atributo.getValue().equals("Psiquiatra")){
+                    valorBusq = valor.getText().trim();
                     buscado = Psiquiatra.psiquiatraHash.get(valorBusq);
-                }else if (Paciente.pacienteHash.containsKey(valor.getText()) && atributo.getValue().equals("Paciente")){
-                    valorBusq = valor.getText();
+                }else if (Paciente.pacienteHash.containsKey(valor.getText().trim()) && atributo.getValue().equals("Paciente")){
+                    valorBusq = valor.getText().trim();
                     for (Object nodo : Graphs.neighborListOf(SistemaDeGestionClinica.BD, Paciente.pacienteHash.get(valorBusq))){
                         if(SistemaDeGestionClinica.identificador(nodo,"psiquiatra")){
                             buscado = (Psiquiatra) nodo;
@@ -175,7 +180,7 @@ public class MenuBusqueda implements Initializable {
                         return;
                     }
                 }
-                novedad.setText("No se encuentra registrado en ninguna clinica.");
+                novedad.setText("No se encuentra registrado en una clinica.");
         }
     }
 
@@ -189,7 +194,7 @@ public class MenuBusqueda implements Initializable {
                         return;
                     }
                 } catch (Exception excep) {
-                    novedad.setText("La cédula es un número, por favor verifique el valor ingresado.");
+                    novedad.setText("Dato inválido, por favor verifique el valor ingresado.");
                     valorBusq = "Dato inválido";
                     return;
                 }
@@ -202,7 +207,7 @@ public class MenuBusqueda implements Initializable {
                 for (String clave : Psiquiatra.psiApell.keySet()) {
                     if (clave.contains(valor.getText().trim().toLowerCase())) {
                         valorBusq = clave;
-                        resultados.appendText(imprimirList(Psiquiatra.psiApell.get(valorBusq).toString()));
+                        resultados.appendText(imprimirLista(Psiquiatra.psiApell.get(valorBusq).toString()));
                     }
                 }
                 break;
@@ -214,17 +219,66 @@ public class MenuBusqueda implements Initializable {
                      return;
                     }
                 } catch (Exception excep) {
-                    novedad.setText("Edad inválida, por favor verifique el valor ingresado.");
+                    novedad.setText("Edad inválida,por favor verifique el valor ingresado.");
                     valorBusq = "Dato inválido.";
                     return;
                 }
                 for (Integer clave : Psiquiatra.psiEdad.keySet()) {
                     if (String.valueOf(clave).contains(valor.getText().trim())) {
                         valorBusq = String.valueOf(clave);
-                        resultados.appendText(imprimirList(Psiquiatra.psiEdad.get(Integer.parseInt(valorBusq)).toString()));
+                        resultados.appendText(imprimirLista(Psiquiatra.psiEdad.get(Integer.parseInt(valorBusq)).toString()));
                     }
                 }
                 break;
+            case "Clinica":
+                try {
+                    if (Integer.parseInt(valor.getText().trim()) < 0){
+                        novedad.setText("El NIT es un número positivo. ");
+                        valorBusq = "Dato inválido.";
+                        return;
+                    }
+                } catch (Exception excep) {
+                    novedad.setText("Dato inválido,por favor verifique el valor ingresado.");
+                    valorBusq = "Dato inválido.";
+                    return;
+                }
+                if (Clinica.ClinicaHash.containsKey(Integer.parseInt(valor.getText()))){
+                    valorBusq = valor.getText().trim();
+                    for (Object nodo : Graphs.neighborListOf(SistemaDeGestionClinica.BD, Clinica.ClinicaHash.get(Integer.parseInt(valorBusq)))){
+                        if(SistemaDeGestionClinica.identificador(nodo,"Psiquiatra")){
+                            Psiquiatra psiquiatra = (Psiquiatra) nodo;
+                            resultados.appendText(psiquiatra.toString() + "\n");
+                        }
+                    }
+                }
+                if (resultados.getText().equals("")){
+                    novedad.setText("No hay psiquiatras registrados en esta clinica.");
+                }
+            case "Paciente":
+                try {
+                    if (Integer.parseInt(valor.getText().trim()) < 0){
+                        novedad.setText("Cédula inválida.");
+                        valorBusq = "Dato inválido";
+                        return;
+                    }
+                } catch (Exception excep) {
+                    novedad.setText("Dato inválido,por favor verifique el valor ingresado.");
+                    valorBusq = "Dato inválido";
+                    return;
+                }
+                if (Paciente.pacienteHash.containsKey(valor.getText().trim())){
+                    valorBusq = valor.getText().trim();
+                    for (Object nodo : Graphs.neighborListOf(SistemaDeGestionClinica.BD, Paciente.pacienteHash.get(valorBusq))){
+                        if(SistemaDeGestionClinica.identificador(nodo,"Psiquiatra")){
+                            Psiquiatra psiquiatra = (Psiquiatra) nodo;
+                            resultados.appendText(psiquiatra.toString());
+                            break;
+                        }
+                    }
+                    if (resultados.getText().equals("")){
+                        novedad.setText("Este paciente no tiene médico asignado.");
+                    }
+                }
         }
     }
 
@@ -238,7 +292,7 @@ public class MenuBusqueda implements Initializable {
                         return;
                     }
                 } catch (Exception excep) {
-                    novedad.setText("La cédula es un número, por favor verifique el valor ingresado.");
+                    novedad.setText("Dato inválido, por favor verifique el valor ingresado.");
                     valorBusq = "Dato inválido";
                     return;
                 }
@@ -250,7 +304,7 @@ public class MenuBusqueda implements Initializable {
                 for (String clave : Paciente.pacienteApel.keySet()) {
                     if (clave.contains(valor.getText().trim().toLowerCase())) {
                         valorBusq = clave;
-                        resultados.appendText(imprimirList(Paciente.pacienteApel.get(valorBusq).toString()));
+                        resultados.appendText(imprimirLista(Paciente.pacienteApel.get(valorBusq).toString()));
                     }
                 }
                 break;
@@ -269,14 +323,67 @@ public class MenuBusqueda implements Initializable {
                for (Integer clave : Paciente.pacienteEdad.keySet()) {
                     if (String.valueOf(clave).contains(valor.getText().trim())) {
                         valorBusq = String.valueOf(clave);
-                        resultados.appendText(imprimirList(Paciente.pacienteEdad.get(Integer.parseInt(valorBusq)).toString()));
+                        resultados.appendText(imprimirLista(Paciente.pacienteEdad.get(Integer.parseInt(valorBusq)).toString()));
                     }
                 }
                 break;
+            case "Clinica":
+                try {
+                    if (Integer.parseInt(valor.getText().trim()) < 0){
+                        novedad.setText("El NIT es un número positivo. ");
+                        valorBusq = "Dato inválido.";
+                        return;
+                    }
+                } catch (Exception excep) {
+                    novedad.setText("Dato inválido, por favor verifique el valor ingresado.");
+                    valorBusq = "Dato inválido.";
+                    return;
+                }
+                if (Clinica.ClinicaHash.containsKey(Integer.parseInt(valor.getText()))){
+                    valorBusq = valor.getText().trim();
+                    for (Object nodo : Graphs.neighborListOf(SistemaDeGestionClinica.BD, Clinica.ClinicaHash.get(Integer.parseInt(valorBusq)))){
+                        if(SistemaDeGestionClinica.identificador(nodo,"Psiquiatra")){
+                            Psiquiatra psiquiatra = (Psiquiatra) nodo;
+                            for (Object nodo2: Graphs.neighborListOf(SistemaDeGestionClinica.BD, psiquiatra)){
+                                if (SistemaDeGestionClinica.identificador(nodo2,"Paciente")){
+                                    Paciente paciente = (Paciente) nodo2;
+                                    resultados.appendText(paciente.toString() + "\n" );
+                                }
+                            }
+                        }
+                    }
+                }
+                if (resultados.getText().equals("")){
+                    novedad.setText("No hay pacientes registrados en esta clinica.");
+                }
+            case "Psiquiatra":
+                try {
+                    if (Integer.parseInt(valor.getText().trim()) < 0){
+                        novedad.setText("Cédula inválida.");
+                        valorBusq = "Dato inválido";
+                        return;
+                    }
+                } catch (Exception excep) {
+                    novedad.setText("Dato inválido, por favor verifique el valor ingresado.");
+                    valorBusq = "Dato inválido";
+                    return;
+                }
+                if (Psiquiatra.psiquiatraHash.containsKey(valor.getText().trim())){
+                    valorBusq = valor.getText().trim();
+                    for (Object nodo : Graphs.neighborListOf(SistemaDeGestionClinica.BD,Psiquiatra.psiquiatraHash.get(valorBusq))){
+                        if (SistemaDeGestionClinica.identificador(nodo,"Paciente")){
+                            Paciente paciente = (Paciente) nodo;
+                            resultados.appendText(paciente.toString() + "\n" );
+                        }
+                    }
+                }
+                if (resultados.getText().equals("")){
+                    novedad.setText("El psiquiatra no tiene pacientes asignados.");
+                }
         }
     }
 
-    public String imprimirList(String list){
+    public String imprimirLista(String list){
          return list.replace(",", "\n").replace("[", "").replace("]", "\n");
     }
 
--- src/main/java/org/example/SistemaDeGestionClinica.java
+++ src/main/java/org/example/SistemaDeGestionClinica.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.jgrapht.Graph;
-import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 import java.io.IOException;
@@ -45,12 +44,10 @@ public class SistemaDeGestionClinica extends Application {
         Clinica clinica3 = new Clinica(3, "REMY IPS", "Cra. 50A #59-56", 1011121);
         Clinica clinica4 = new Clinica(4, "Clinica Sagrado Corazón", "Cra.50 #62-63", 123456);
 
-
         //Paciente.
         Paciente paciente1 = new Paciente("1000194793","Ximena","Castañeda Ochoa","Ximena@gmail.com","123","Cra. 45 #72-56",19,"05/10/2001","7865744","Carlos Lopez","2631551");
         Paciente paciente2 = new Paciente("1008194713","Felipe","Quintero Cano","Felipe@gmail.com","456","Cl. 78 #86-40",25,"14/03/1995","3193274553","Sofia Velez","4181376");
         Paciente paciente3 = new Paciente("1078694724","Juan Felipe","Rojas Calderon","Juan@gmail.com","789","Cra. 77A #50-30",25,"05/10/1995","5809296","Camila Aguirre","3931851");
-
 
         //Psiquiatra.
         Psiquiatra Psiquiatra1 = new Psiquiatra("1017252071","Andrés Felipe","García Revuelta","Andres@gmail.com","147","Masculino","Cl. 80 #54-20",23,"07/01/1993",8790344);
@@ -59,9 +56,9 @@ public class SistemaDeGestionClinica extends Application {
         Psiquiatra Psiquiatra4 = new Psiquiatra("946145641","Felipe","García Revuelta","Felipe@gmail.com","777","Masculino","Cl. 80 #54-20",23,"10/01/1993",8797536);
 
         //Relaciones.
-        BD.addEdge(coordinador, clinica1);
-        BD.addEdge(coordinador, clinica2);
-        BD.addEdge(coordinador, clinica3);
+        BD.addEdge(coordinador,clinica1);
+        BD.addEdge(coordinador,clinica2);
+        BD.addEdge(coordinador,clinica3);
         BD.addEdge(coordinador,clinica4);
         BD.addEdge(clinica1,Psiquiatra1);
         BD.addEdge(clinica2,Psiquiatra2);
